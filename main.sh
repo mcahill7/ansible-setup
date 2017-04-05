57da85f19ea17219e0432568307b4c08ff722f9f
Mason Cahill <mason.j.cahill@gmail.com>
2017-04-05 10:44:29 -0400

    2nd commit

---
+++ main.sh
@@ -0,0 +1,31 @@
+#install homebrew
+if [ ! -x /usr/local/bin/brew ]; then
+    echo "installing homebrew"
+    /usr/bin/env ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "homebrew is installed"
+fi
+
+if [ ! -x /usr/local/bin/ansible ]; then
+    echo "installing ansible via homebrew"
+    brew install ansible
+else
+    echo "ansible is installed"
+fi
+
+#brew
+sudo chown -R $(whoami) /usr/local
+sudo chown -R ${USER}:staff /opt/homebrew-cask
+brew doctor
+brew update
+brew upgrade
+brew cleanup
+brew cask cleanup
+brew link gdbm
+brew install wget
+brew install python
+pip install --upgrade pip setuptools
+
+#run playbook
+sudo ansible-galaxy install geerlingguy.homebrew --ignore-certs
+ansible-playbook pb.yml
